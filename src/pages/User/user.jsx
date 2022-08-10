3435e2f0f567bacff20dcf579c0ff7ad2bac1f00
ludogwada <83616314+ludogwada@users.noreply.github.com>
2022-08-10 10:23:57 -0400

    feature: update user file

--- src/pages/User/user.jsx
+++ src/pages/User/user.jsx
@@ -1,20 +1,88 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import transaction from '../../data/transactions.json';
 import Card from '../../components/Card/card';
+import { useState } from 'react';
+import callApi from '../../utils/Api/callApi';
+import { getUser } from '../../utils/Slice/slice';
 
 function User() {
-	const firstName = useSelector((state) => state.connect.firstName);
-	const lastName = useSelector((state) => state.connect.lastName);
+	const stateFirstName = useSelector((state) => state.connect.firstName);
+	const stateLastName = useSelector((state) => state.connect.lastName);
+	const tokenUser = useSelector((state) => state.connect.token);
+	const [firstName, setFirstName] = useState();
+	const [lastName, setLastName] = useState();
+	const [displayEditUser, setDisplayEditUser] = useState(false);
+	const dispatch = useDispatch();
+
+	const displayFormEdit = () => {
+		setDisplayEditUser(!displayEditUser);
+	};
+
+	const editUser = async () => {
+		const responseApi = await callApi.putUser(tokenUser, {
+			firstName,
+			lastName,
+		});
+		dispatch(
+			getUser({
+				firstName: responseApi.firstName,
+				lastName: responseApi.lastName,
+			})
+		);
+		setDisplayEditUser(!displayEditUser);
+	};
 
 	return (
-		<main className='main bg-dark'>
+		<main className='main bg-dark mainUser'>
 			<div className='user-header'>
-				<h1>
-					Welcome back
-					<br />
-					{firstName + ' ' + lastName}!
-				</h1>
-				<button className='edit-button'>Edit Name</button>
+				{displayEditUser ? (
+					<>
+						<h1>Welcome back</h1>
+						<form>
+							<div className='labelEdit'>
+								<label htmlFor='firstName'>
+									<input
+										placeholder='First Name'
+										type='text'
+										onChange={(e) => setFirstName(e.target.value)}
+									/>
+								</label>
+								<label htmlFor='lastName'>
+									<input
+										placeholder='Last Name'
+										type='text'
+										onChange={(e) => setLastName(e.target.value)}
+									/>
+								</label>
+							</div>
+							<div>
+								<button
+									className='edit-button'
+									type='button'
+									onClick={() => displayFormEdit()}>
+									Cancel
+								</button>
+								<button
+									className='edit-button'
+									type='button'
+									onClick={() => editUser()}>
+									Save
+								</button>
+							</div>
+						</form>
+					</>
+				) : (
+					<>
+						<h1>
+							Welcome back
+							<br />
+							{stateFirstName + ' ' + stateLastName}!
+						</h1>
+						<button className='edit-button' onClick={() => displayFormEdit()}>
+							Edit Name
+						</button>
+					</>
+				)}
 			</div>
 			<h2 className='sr-only'>Accounts</h2>
 			{transaction.map((account) => {
